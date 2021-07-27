6b778978f30b77f469b14a419249e54a44c86ef7
steve <dev@somecool.net>
2021-07-27 11:21:16 -0700

    add Default implementation for HttpResponse (status 200, empty body and headers)

--- httpserver/rust/src/lib.rs
+++ httpserver/rust/src/lib.rs
@@ -2,3 +2,13 @@
 
 mod httpserver;
 pub use httpserver::*;
+
+impl Default for HttpResponse {
+    fn default() -> HttpResponse {
+        HttpResponse {
+            status_code: 200,
+            body: Vec::default(),
+            header: HeaderMap::default(),
+        }
+    }
+}
