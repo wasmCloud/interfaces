ca6c8dc8ab4ccbe57338393a68bd84603b7ff945
steve <dev@somecool.net>
2021-07-27 11:26:16 -0700

    default implementation of HttpResposne
    Signed-off-by: steve <dev@somecool.net>

--- httpserver/rust/src/lib.rs
+++ httpserver/rust/src/lib.rs
@@ -4,6 +4,7 @@ mod httpserver;
 pub use httpserver::*;
 
 impl Default for HttpResponse {
+    /// create default HttpResponse with status 200, empty body and empty header
     fn default() -> HttpResponse {
         HttpResponse {
             status_code: 200,
