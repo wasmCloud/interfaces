dba9c25664a15c24d0039c0f6558bec3882a384e
stevelr <steve@cosmonic.com>
2021-09-03 13:10:55 -0700

    regerate with actorReceive fixes
    Signed-off-by: stevelr <steve@cosmonic.com>

--- httpclient/rust/src/httpclient.rs
+++ httpclient/rust/src/httpclient.rs
@@ -118,16 +118,6 @@ impl<T: Transport> HttpClientSender<T> {
         Self { transport }
     }
 }
-#[cfg(not(target_arch = "wasm32"))]
-impl<'send> HttpClientSender<wasmbus_rpc::provider::ProviderTransport<'send>> {
-    /// Constructs a Sender using an actor's LinkDefinition,
-    /// Uses the provider's HostBridge for rpc
-    pub fn for_actor(ld: &'send wasmbus_rpc::core::LinkDefinition) -> Self {
-        Self {
-            transport: wasmbus_rpc::provider::ProviderTransport::new(ld, None),
-        }
-    }
-}
 
 #[cfg(target_arch = "wasm32")]
 impl HttpClientSender<wasmbus_rpc::actor::prelude::WasmHost> {
@@ -146,14 +136,6 @@ impl HttpClientSender<wasmbus_rpc::actor::prelude::WasmHost> {
         let transport =
             wasmbus_rpc::actor::prelude::WasmHost::to_provider("wasmcloud:httpclient", link_name)?;
         Ok(Self { transport })
-    }
-
-    /// Constructs a client for actor-to-actor messaging
-    /// using the recipient actor's public key
-    pub fn to_actor(actor_id: &str) -> Self {
-        let transport =
-            wasmbus_rpc::actor::prelude::WasmHost::to_actor(actor_id.to_string()).unwrap();
-        Self { transport }
     }
 }
 #[async_trait]
