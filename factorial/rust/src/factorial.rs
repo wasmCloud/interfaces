fa0cd1a6c7d587e955b14596b072e55b94b40f6c
stevelr <steve@cosmonic.com>
2021-09-03 13:09:03 -0700

    regenerate
    Signed-off-by: stevelr <steve@cosmonic.com>

--- factorial/rust/src/factorial.rs
+++ factorial/rust/src/factorial.rs
@@ -72,6 +72,7 @@ impl<T: Transport> FactorialSender<T> {
         Self { transport }
     }
 }
+
 #[cfg(not(target_arch = "wasm32"))]
 impl<'send> FactorialSender<wasmbus_rpc::provider::ProviderTransport<'send>> {
     /// Constructs a Sender using an actor's LinkDefinition,
@@ -80,6 +81,16 @@ impl<'send> FactorialSender<wasmbus_rpc::provider::ProviderTransport<'send>> {
         Self {
             transport: wasmbus_rpc::provider::ProviderTransport::new(ld, None),
         }
+    }
+}
+#[cfg(target_arch = "wasm32")]
+impl FactorialSender<wasmbus_rpc::actor::prelude::WasmHost> {
+    /// Constructs a client for actor-to-actor messaging
+    /// using the recipient actor's public key
+    pub fn to_actor(actor_id: &str) -> Self {
+        let transport =
+            wasmbus_rpc::actor::prelude::WasmHost::to_actor(actor_id.to_string()).unwrap();
+        Self { transport }
     }
 }
 
@@ -104,14 +115,6 @@ impl FactorialSender<wasmbus_rpc::actor::prelude::WasmHost> {
             link_name,
         )?;
         Ok(Self { transport })
-    }
-
-    /// Constructs a client for actor-to-actor messaging
-    /// using the recipient actor's public key
-    pub fn to_actor(actor_id: &str) -> Self {
-        let transport =
-            wasmbus_rpc::actor::prelude::WasmHost::to_actor(actor_id.to_string()).unwrap();
-        Self { transport }
     }
 }
 #[async_trait]
