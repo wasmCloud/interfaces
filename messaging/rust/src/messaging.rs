a05c7931ea688b12a3682140ea46c40028a2dcdb
stevelr <steve@cosmonic.com>
2021-10-23 23:19:09 -0700

    restore messaging folder - was accidentally deleted
    Signed-off-by: stevelr <steve@cosmonic.com>

--- messaging/rust/src/messaging.rs
+++ messaging/rust/src/messaging.rs
@@ -1,17 +1,13 @@
-// This file is generated automatically using wasmcloud-weld and smithy model definitions
+// This file is generated automatically using wasmcloud/weld-codegen and smithy model definitions
 //
 
-#![allow(clippy::ptr_arg)]
-#[allow(unused_imports)]
+#![allow(unused_imports, clippy::ptr_arg, clippy::needless_lifetimes)]
 use async_trait::async_trait;
-#[allow(unused_imports)]
 use serde::{Deserialize, Serialize};
-#[allow(unused_imports)]
-use std::{borrow::Cow, string::ToString};
-#[allow(unused_imports)]
+use std::{borrow::Cow, io::Write, string::ToString};
 use wasmbus_rpc::{
     deserialize, serialize, Context, Message, MessageDispatch, RpcError, RpcResult, SendOpts,
-    Transport,
+    Timestamp, Transport,
 };
 
 pub const SMITHY_VERSION: &str = "1.0";
@@ -19,45 +15,45 @@ pub const SMITHY_VERSION: &str = "1.0";
 /// A message to be published
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub struct PubMessage {
-    /// The message payload
-    #[serde(with = "serde_bytes")]
+    /// The subject, or topic, of the message
     #[serde(default)]
-    pub body: Vec<u8>,
+    pub subject: String,
     /// An optional topic on which the reply should be sent.
     #[serde(rename = "replyTo")]
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub reply_to: Option<String>,
-    /// The subject, or topic, of the message
+    /// The message payload
+    #[serde(with = "serde_bytes")]
     #[serde(default)]
-    pub subject: String,
+    pub body: Vec<u8>,
 }
 
 /// Reply received from a Request operation
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub struct ReplyMessage {
-    /// The message payload
-    #[serde(with = "serde_bytes")]
+    /// The subject, or topic, of the message
     #[serde(default)]
-    pub body: Vec<u8>,
+    pub subject: String,
     /// An optional topic on which the reply should be sent.
     #[serde(rename = "replyTo")]
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub reply_to: Option<String>,
-    /// The subject, or topic, of the message
+    /// The message payload
+    #[serde(with = "serde_bytes")]
     #[serde(default)]
-    pub subject: String,
+    pub body: Vec<u8>,
 }
 
 /// Message sent as part of a request, with timeout
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub struct RequestMessage {
+    /// The subject, or topic, of the message
+    #[serde(default)]
+    pub subject: String,
     /// The message payload
     #[serde(with = "serde_bytes")]
     #[serde(default)]
     pub body: Vec<u8>,
-    /// The subject, or topic, of the message
-    #[serde(default)]
-    pub subject: String,
     /// A timeout, in milliseconds
     #[serde(rename = "timeoutMs")]
     pub timeout_ms: u32,
@@ -66,17 +62,17 @@ pub struct RequestMessage {
 /// Message received as part of a subscription
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub struct SubMessage {
-    /// The message payload
-    #[serde(with = "serde_bytes")]
+    /// The subject, or topic, of the message
     #[serde(default)]
-    pub body: Vec<u8>,
+    pub subject: String,
     /// An optional topic on which the reply should be sent.
     #[serde(rename = "replyTo")]
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub reply_to: Option<String>,
-    /// The subject, or topic, of the message
+    /// The message payload
+    #[serde(with = "serde_bytes")]
     #[serde(default)]
-    pub subject: String,
+    pub body: Vec<u8>,
 }
 
 /// The MessageSubscriber interface describes
@@ -106,11 +102,11 @@ pub trait MessageSubscriberReceiver: MessageDispatch + MessageSubscriber {
             "HandleMessage" => {
                 let value: SubMessage = deserialize(message.arg.as_ref())
                     .map_err(|e| RpcError::Deser(format!("message '{}': {}", message.method, e)))?;
-                let resp = MessageSubscriber::handle_message(self, ctx, &value).await?;
-                let buf = Cow::Owned(serialize(&resp)?);
+                let _resp = MessageSubscriber::handle_message(self, ctx, &value).await?;
+                let buf = Vec::new();
                 Ok(Message {
                     method: "MessageSubscriber.HandleMessage",
-                    arg: buf,
+                    arg: Cow::Owned(buf),
                 })
             }
             _ => Err(RpcError::MethodNotHandled(format!(
@@ -135,6 +131,10 @@ impl<T: Transport> MessageSubscriberSender<T> {
     /// Constructs a MessageSubscriberSender with the specified transport
     pub fn via(transport: T) -> Self {
         Self { transport }
+    }
+
+    pub fn set_timeout(&self, interval: std::time::Duration) {
+        self.transport.set_timeout(interval);
     }
 }
 
@@ -165,14 +165,14 @@ impl<T: Transport + std::marker::Sync + std::marker::Send> MessageSubscriber
     #[allow(unused)]
     /// subscription handler
     async fn handle_message(&self, ctx: &Context, arg: &SubMessage) -> RpcResult<()> {
-        let arg = serialize(arg)?;
+        let buf = serialize(arg)?;
         let resp = self
             .transport
             .send(
                 ctx,
                 Message {
                     method: "MessageSubscriber.HandleMessage",
-                    arg: Cow::Borrowed(&arg),
+                    arg: Cow::Borrowed(&buf),
                 },
                 None,
             )
@@ -213,21 +213,21 @@ pub trait MessagingReceiver: MessageDispatch + Messaging {
             "Publish" => {
                 let value: PubMessage = deserialize(message.arg.as_ref())
                     .map_err(|e| RpcError::Deser(format!("message '{}': {}", message.method, e)))?;
-                let resp = Messaging::publish(self, ctx, &value).await?;
-                let buf = Cow::Owned(serialize(&resp)?);
+                let _resp = Messaging::publish(self, ctx, &value).await?;
+                let buf = Vec::new();
                 Ok(Message {
                     method: "Messaging.Publish",
-                    arg: buf,
+                    arg: Cow::Owned(buf),
                 })
             }
             "Request" => {
                 let value: RequestMessage = deserialize(message.arg.as_ref())
                     .map_err(|e| RpcError::Deser(format!("message '{}': {}", message.method, e)))?;
                 let resp = Messaging::request(self, ctx, &value).await?;
-                let buf = Cow::Owned(serialize(&resp)?);
+                let buf = serialize(&resp)?;
                 Ok(Message {
                     method: "Messaging.Request",
-                    arg: buf,
+                    arg: Cow::Owned(buf),
                 })
             }
             _ => Err(RpcError::MethodNotHandled(format!(
@@ -251,6 +251,10 @@ impl<T: Transport> MessagingSender<T> {
     /// Constructs a MessagingSender with the specified transport
     pub fn via(transport: T) -> Self {
         Self { transport }
+    }
+
+    pub fn set_timeout(&self, interval: std::time::Duration) {
+        self.transport.set_timeout(interval);
     }
 }
 
@@ -282,14 +286,14 @@ impl<T: Transport + std::marker::Sync + std::marker::Send> Messaging for Messagi
     /// the replyTo field should be filled with the
     /// subject for the response.
     async fn publish(&self, ctx: &Context, arg: &PubMessage) -> RpcResult<()> {
-        let arg = serialize(arg)?;
+        let buf = serialize(arg)?;
         let resp = self
             .transport
             .send(
                 ctx,
                 Message {
                     method: "Messaging.Publish",
-                    arg: Cow::Borrowed(&arg),
+                    arg: Cow::Borrowed(&buf),
                 },
                 None,
             )
@@ -300,14 +304,14 @@ impl<T: Transport + std::marker::Sync + std::marker::Send> Messaging for Messagi
     /// Request - send a message in a request/reply pattern,
     /// waiting for a response.
     async fn request(&self, ctx: &Context, arg: &RequestMessage) -> RpcResult<ReplyMessage> {
-        let arg = serialize(arg)?;
+        let buf = serialize(arg)?;
         let resp = self
             .transport
             .send(
                 ctx,
                 Message {
                     method: "Messaging.Request",
-                    arg: Cow::Borrowed(&arg),
+                    arg: Cow::Borrowed(&buf),
                 },
                 None,
             )
